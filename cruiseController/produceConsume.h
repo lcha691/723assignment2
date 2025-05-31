86fc820904ecfbb201ad263711a7d3aec2145ca7
Lachlan Chan <lcha691@flexitubuntu2004-template-tdc.its.auckland.ac.nz>
2025-05-31 20:25:57 +1200

    Making some form of progress

--- cruiseController/produceConsume.h
+++ cruiseController/produceConsume.h
@@ -1,7 +1,21 @@
 //The type 'string' is defined as 'char*' in Esterel, hence passing a string by referenc  is 'char**'
 //Aviod naming a function 'recv'
-void send(int data);
-void recv1(char** data);
-void adding(unsigned int data);
-int remov();
-int outCount();
+// void send(int data);
+// void recv1(char** data);
+// void adding(unsigned int data);
+// int remov();
+// int outCount();
+
+#include <stdbool.h>
+
+#define PedalsMin 3.00
+#define SpeedMin 30.00
+#define SpeedMax 150.00
+#define Zero 0.00
+
+
+
+float saturateThrottle(float throttleIn, bool* saturate);
+
+
+// float regulateThrottle(bool isGoingOn, float cruiseSpeed, float vehicleSpeed); // for some reason I need to comment this out
--- cruiseController/produceConsume_data.c
+++ cruiseController/produceConsume_data.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include "produceConsume.h"
+// #include "produceConsume.h"
 #define SIZE 5
 
 int cq[SIZE];
@@ -43,4 +43,64 @@ int remov(void){
 	}else
 		return -1;
 
+}
+
+#include <stdbool.h>
+
+/*
+DESCRIPTION: Saturate the throttle command to limit the acceleration.
+PARAMETERS: throttleIn - throttle input
+            saturate - true if saturated, false otherwise
+RETURNS: throttle output (ThrottleCmd)
+*/
+float saturateThrottle(float throttleIn, bool* saturate)
+{
+	static const float THROTTLESATMAX = 45.0;
+	if (throttleIn > THROTTLESATMAX) {
+		*saturate = true;
+		return THROTTLESATMAX;
+	}
+	else if (throttleIn < 0) {
+		*saturate = true;
+		return 0;
+	}
+	else {
+		*saturate = false;
+		return throttleIn;
+	}
+}
+
+/*
+DESCRIPTION: Saturate the throttle command to limit the acceleration.
+PARAMETERS: isGoingOn - true if the cruise control has just gone into the ON state 
+                        from another state; false otherwise
+            saturate - true if saturated, false otherwise
+RETURNS: throttle output (ThrottleCmd)
+*/
+float regulateThrottle(bool isGoingOn, float cruiseSpeed, float vehicleSpeed)
+{
+	static const float KP = 8.113;
+	static const float KI = 0.5;
+	static bool saturate = true;
+	static float iterm = 0;
+	
+	if (isGoingOn) {
+		iterm = 0;	// reset the integral action
+		saturate = true;	
+	}
+	float error = cruiseSpeed - vehicleSpeed;
+	float proportionalAction = error * KP;
+	if (saturate)
+		error = 0;	// integral action is hold when command is saturated
+	iterm = iterm + error;
+	float integralAction = KI * iterm;
+	return saturateThrottle(proportionalAction + integralAction, &saturate);
+}
+
+int number(int num){
+	return num;
+}
+
+float floatCal(float f){
+return f;
 }
